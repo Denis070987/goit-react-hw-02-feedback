9d93b168e254d43769c807ce06cbb28ff0b504cc
Denis Boyko <deniskoms@gmail.com>
2023-01-14 22:19:35 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,23 @@
-import { Component } from "react";
+// import { Component } from "react";
 
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0
+//   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
+//   countTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   };
 
-  countPositiveFeedbackPercentage = evt => {
-    return ((this.state.good * 100) / this.countTotalFeedback)
-  };
+//   countPositiveFeedbackPercentage = evt => {
+//     return ((this.state.good * 100) / this.countTotalFeedback)
+//   };
 
-  leaveFeedback = evt => {
-    const name = evt.target.name
-  }
-} 
+//   leaveFeedback = evt => {
+//     const name = evt.target.name
+//   }
+// } 
